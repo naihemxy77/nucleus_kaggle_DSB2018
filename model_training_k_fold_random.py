3333c4deb0e4aec37a8319910b715532b1664ba4
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-03-12 20:49:48 -0400

    training with random sample sequence

---
+++ model_training_k_fold_random.py
@@ -0,0 +1,83 @@
+from RandomGenClass import DataGenerator
+import InputOutputForNN as ionn
+import pandas as pd
+import numpy as np
+import cnn_shallow_0307 as nn_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint, History
+import h5py
+import pickle
+from sklearn.model_selection import KFold
+import random
+
+#Train Test Split parameters
+n = 5
+id_num = 'Guo_0312_shallow_'+str(n)+'fold'
+SEED = 932894
+#Confidence threshold for nuclei identification
+cutoff = 0.5
+
+train_df = pickle.load(open("../input/train_df.p","rb"))
+random.seed(124335)
+#Fragment parameters
+InputDim = [128,128]
+OutputDim = [100,100]
+Stride = [50,50]
+#Extract train data imageids
+train_df = train_df
+total_ids = list(train_df['ImageId'].values)
+#If just want to train fluorescent data (similarly, 1 for histo and 2 for bright)
+#train_df = train_df[train_df['hsv_cluster']==0]
+#total_ids = list(train_df.loc[train_df['hsv_cluster']==0,'ImageId'].values)
+
+#Split images into cross-fold sets (note that pieces for one image always together belong to train/val set)
+kf = KFold(n_splits=n, shuffle=True, random_state=SEED)
+ids=list(kf.split(total_ids))
+loc_i = np.arange(n)
+
+def model_fitting(ids,I,train_df):
+    global Test_data
+    #concatenate all pieces into one dataset
+    train_ids = [total_ids[i] for i in ids[0]]
+    val_ids = [total_ids[i] for i in ids[1]]
+    #model fitting
+    model = nn_model.model_gen(InputDim)
+    epochs_number = 10
+    batch_size = 32
+    earlyStopping = EarlyStopping(monitor='val_loss', patience=3, verbose=0, mode='min')
+    mcp_save = ModelCheckpoint('model_'+str(id_num)+'_'+str(I)+'.hdf5', save_best_only=True, monitor='val_loss', mode='min')
+    history = History()
+    params ={'dim_x': InputDim[0],
+             'dim_y': InputDim[1],
+             'dim_z': 3,
+             'batch_size': batch_size,
+             'shuffle': True}
+    training_generator = DataGenerator(**params).generate(train_ids,train_df)
+    validation_generator = DataGenerator(**params).generate(val_ids,train_df)
+    model.fit_generator(generator=training_generator, steps_per_epoch=len(train_ids)//batch_size, epochs=epochs_number, validation_data=validation_generator,validation_steps=len(val_ids)//batch_size, callbacks=[earlyStopping,mcp_save,history])
+    df = pd.DataFrame.from_dict(history.history)
+    df.to_csv('history_'+str(id_num)+'_'+str(I)+'.csv', sep='\t', index=True, float_format='%.4f')
+    #test data prediction
+    model.load_weights(filepath = 'model_'+str(id_num)+'_'+str(I)+'.hdf5')
+    Test_Label_I = []
+    for t in range(Test_data.shape[0]):
+        test_x = Test_data.loc[t,'X']
+        pred_test = model.predict(test_x)
+        Test_Label_I.append(pred_test)
+    return Test_Label_I
+
+#Import test data pieces given image type: 'all','fluo','histo' or 'bright'
+Test_data = ionn.sub_fragments_extract(InputDim=InputDim,OutputDim=OutputDim,Stride=Stride,image_type='all',train=False,reflection=False)
+
+pred_outputs_kfold = []
+for i in range(n):
+    pred_outputs_kfold.append(model_fitting(ids[i],i,train_df))
+pred_outputs_kfold = np.sum(pred_outputs_kfold,axis=0)/n
+
+Test_Label = []
+for i in range(Test_data.shape[0]):
+    pred_test = pred_outputs_kfold[i]
+    pred_label = ionn.MidExtractProcess(pred_test,InputDim[0],InputDim[1],OutputDim[0],OutputDim[1])
+    OutputImage = ionn.OutputStitch(img_shape=Test_data.loc[i,'ImageShape'],output=pred_label,strideX=Stride[0],strideY=Stride[1])
+    OutputImage = np.where(OutputImage>cutoff,1,0)
+    Test_Label.append((Test_data.loc[i,'ImageId'],OutputImage))
+pickle.dump(Test_Label,open( "Test_Label.p","wb" ))
