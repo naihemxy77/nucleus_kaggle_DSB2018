3460da322f4002d560df41b356d6ba35f4b29737
naihemxy77 <36382731+naihemxy77@users.noreply.github.com>
2018-03-18 14:10:36 -0400

    Update model_training_k_fold_random.py

--- model_training_k_fold_random.py
+++ model_training_k_fold_random.py
@@ -80,7 +80,7 @@ for i in range(n):
     model_fitting(ids[i],i,train_df)
 
 #Import test data pieces given image type: 'all','fluo','histo' or 'bright'
-Test_data = ionn.sub_fragments_extract(InputDim=InputDim,OutputDim=OutputDim,Stride=Stride,image_type='all',train=False,reflection=False)
+Test_data = ionn.sub_fragments_extract(InputDim=InputDim,OutputDim=OutputDim,Stride=Stride,image_type='histo',train=False,reflection=False)
 print('Start to predict...')
 #pred_outputs_kfold = []
 for i in range(n):
