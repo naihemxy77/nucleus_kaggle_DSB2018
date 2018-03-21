c042c899c07098820a793638051563c966e2c533
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-03-20 20:52:00 -0400

    Update model_training_k_fold_random.py

--- model_training_k_fold_random.py
+++ model_training_k_fold_random.py
@@ -2,7 +2,7 @@ from RandomGenClass import DataGenerator
 import InputOutputForNN as ionn
 import pandas as pd
 import numpy as np
-import cnn_deep_0313 as nn_model
+import ZoomNet_0320 as nn_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint, History
 import h5py
 import pickle
@@ -11,7 +11,7 @@ import random
 
 #Train Test Split parameters
 n = 5
-id_num = 'Guo_0315_deep_'+str(n)+'fold'
+id_num = 'Guo_0320_ZoomNet_'+str(n)+'fold'
 SEED = 932894
 #Confidence threshold for nuclei identification
 cutoff = 0.5
@@ -40,7 +40,7 @@ def model_fitting(ids,I,train_df):
     val_ids = [total_ids[i] for i in ids[1]]
     #model fitting
     model = nn_model.model_gen(InputDim)
-    epochs_number = 30
+    epochs_number = 50
     batch_size = 32
     earlyStopping = EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='min')
     mcp_save = ModelCheckpoint('model_'+str(id_num)+'_'+str(I)+'.hdf5', save_best_only=True, monitor='val_loss', mode='min')
