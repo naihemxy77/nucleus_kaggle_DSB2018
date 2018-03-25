85e0ecb4c99a92825cb9bd6390c487a8c88061be
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-03-24 20:27:08 -0400

    Update submission_gen_given_test_label.py

--- submission_gen_given_test_label.py
+++ submission_gen_given_test_label.py
@@ -12,7 +12,7 @@ import AggressiveSplit as sn
 ##Generate Test Masks and Submission files given pickle outputs from models
 ##In current file, if image type is not histological, then test masks will be
 ##predicted by the linear combination of otsu,iso and li.
-submissionfilename = 'ZoomHeyNet_submission0323_invert'
+submissionfilename = 'ZoomNet_0324_invert_dp15'
 test_label = pickle.load(open( "Test_Label.p","rb" ))
 test_label = pd.DataFrame(test_label, columns=['ImageId','ImageOutput'])
 
