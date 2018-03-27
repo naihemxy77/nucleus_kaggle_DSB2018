d0b5176d4f46965a6de363f25994927c18b38598
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-03-27 17:45:05 -0400

    Add files via upload

--- submission_gen_given_test_label.py
+++ submission_gen_given_test_label.py
@@ -7,27 +7,46 @@ import submission_encoding
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib.backends.backend_pdf import PdfPages
-import AggressiveSplit as sn
+import PerfectSplit as sn
+from skimage.morphology import square,binary_closing
 
+cutoff = 0.5
 ##Generate Test Masks and Submission files given pickle outputs from models
 ##In current file, if image type is not histological, then test masks will be
 ##predicted by the linear combination of otsu,iso and li.
-submissionfilename = 'ZoomNet_0326_invert_data_aug'
+submissionfilename = 'ZoomNet_invert_test_rot'
 test_label = pickle.load(open( "Test_Label.p","rb" ))
 test_label = pd.DataFrame(test_label, columns=['ImageId','ImageOutput'])
 
+test_label_rot = pickle.load(open( "Test_Label_rot.p","rb" ))
+test_label_rot = pd.DataFrame(test_label_rot, columns=['ImageId','ImageOutput'])
+
+selected_id = [12,19,22,44,49,51,56,62]#[5,7,12,14,19,22,24,44,49,51,56,62]
 final_label = []
 for i in range(test_label.shape[0]):
+    #if i in selected_id:
     print(str(i)+"th image is being processed...")
     img = test_label.loc[i,'ImageOutput']
-    if np.sum(img)>0.9*img.shape[0]*img.shape[1]:
-        img = np.where(img>0,0,1)
-        img[:5,:5] = 1
-    if img.max()==0:
-        img[:5,:5] = 1
-    label_i = sn.aggressiveLabel(img.squeeze())
+    img_rot = test_label_rot.loc[i,'ImageOutput']
+    if test_label.loc[i,'ImageId'] == test_label_rot.loc[i,'ImageId']:
+        tmp = np.rot90(img_rot,k=1,axes=(1,0))
+        img_combined = img+tmp
+    else:
+        img_combined = img
+        print(str(i)+'th labels do not match with each other!!!')
+    img_combined = np.where(img_combined>cutoff,1,0)
+    
+    if np.sum(img_combined)>0.9*img_combined.shape[0]*img_combined.shape[1]:
+        img_combined = np.where(img_combined>0,0,1)
+        img_combined[:5,:5] = 1
+    if img_combined.max()==0:
+        img_combined[:5,:5] = 1
+    label_i = sn.aggressiveLabel(img_combined)
     final_label.append(label_i)
+#test_label = test_label.loc[selected_id,:]
+#test_label = test_label.reset_index(drop=True)
 test_label['ImageLabel'] = final_label
+del final_label
 
 r_n = 10
 c_n = 7
