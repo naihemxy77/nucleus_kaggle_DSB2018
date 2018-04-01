504157e137cfef373564d97406ce401ba8135aac
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-04-01 19:20:28 -0400

    Add files via upload

--- data_norm.py
+++ data_norm.py
@@ -27,12 +27,33 @@ def invert_norm(img):
     img = 1-img
     return img
 
-def minmax_norm_extend(img,color_num=2):
+def bg_extend(img,color_num=2):
     colors = get_domimant_colors(img,top_colors=color_num)
     colors=sorted(colors, key=lambda x: (-x[0], x[1]))
     domi = np.zeros((img.shape[0],img.shape[1],img.shape[2]*color_num))
     for i in range(color_num):
-        domi[:,:,i*img.shape[2]:(i+1)*img.shape[2]] = colors[i][1][:3]
-    img = np.concatenate((img,domi),axis=-1)
+        tmp = colors[i][1][:3]
+        tmp2 = []
+        for k in range(len(tmp)):
+            tmp2.append(round(tmp[k]))
+        domi[:,:,i*img.shape[2]:(i+1)*img.shape[2]] = np.array(tmp2).astype('int')
+    img_extend = np.concatenate((img,domi),axis=-1)
+    return img_extend
+
+def minmax_norm_extend(img,color_num=2):
+    img = bg_extend(img,color_num=color_num)
     img_norm = (img-img.min())/(img.max()-img.min())
     return img_norm
+
+def img_extend(df,color_num=2):
+    df_extend = df.copy(deep=True)
+    img_list = []
+    i = 0
+    for index,row in df.iterrows():
+        i = i+1
+        print(str(i)+'th image is processing for image extension...')
+        img = row['Image']
+        img_extend = bg_extend(img,color_num=color_num)
+        img_list.append(img_extend)
+    df_extend['Image'] = img_list
+    return df_extend
