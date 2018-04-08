7a0ac71ec00bb02cce75c19aba5750c501cb7466
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-04-08 17:10:46 -0400

    Update ZoomNet_jac_0402.py

--- ZoomNet_jac_0402.py
+++ ZoomNet_jac_0402.py
@@ -4,6 +4,7 @@ from keras.layers import Input, Conv2D, Dropout,BatchNormalization, Activation
 from keras.optimizers import Adam
 from keras.layers.merge import concatenate
 from keras import backend as K
+import tensorflow as tf
 
 np.random.seed(534899574)
 
@@ -18,14 +19,22 @@ def jaccard_coef(y_true, y_pred):
 def jaccard_loss(y_true, y_pred):
     return 1-jaccard_coef(y_true, y_pred)
 
+def iou(y_pred,y_true):
+    logits=tf.reshape(y_pred, [-1])
+    trn_labels=tf.reshape(y_true, [-1])
+    inter=tf.reduce_sum(tf.multiply(logits,trn_labels))
+    union=tf.reduce_sum(tf.subtract(tf.add(logits,trn_labels),tf.multiply(logits,trn_labels)))
+    loss=tf.divide(inter,union)#tf.subtract(tf.constant(1.0, dtype=tf.float32),tf.divide(inter,union))
+    return loss
+
 def combined_loss(y_true, y_pred):
     binary_loss = K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)#-K.mean(y_true*K.log(y_pred)+(1-y_true)*K.log(1-y_pred))
-    jac = jaccard_coef(y_true, y_pred)
+    jac = iou(y_pred,y_true)
     combined_loss = binary_loss-K.log(jac)
     return combined_loss
 
 def cnn_block(X,f,k):
-    X = Conv2D(filters=f, kernel_size=(k,k), kernel_initializer='he_normal', padding='same', activation='relu')(X)
+    X = Conv2D(filters=f, kernel_size=(k,k), kernel_initializer='he_normal', padding='same')(X)
     X = BatchNormalization()(X)
     X = Activation('relu')(X)
     return X
@@ -51,7 +60,7 @@ def model_gen(InputDim):
     outputs = Conv2D(1,(1,1), kernel_initializer='he_normal', activation='sigmoid')(combined)
     model = Model(inputs=[inputs], outputs=[outputs])
     adam = Adam(lr=0.0005, decay=0.0)
-    model.compile(loss=jaccard_loss,
+    model.compile(loss=combined_loss,
                   optimizer=adam,
                   metrics=['accuracy'])
     print('Model Construction Finished.')
