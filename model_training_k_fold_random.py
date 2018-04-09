6dd5a42da30573920859cc803189aa3ee339fa1e
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-04-09 11:44:01 -0400

    Update model_training_k_fold_random.py

--- model_training_k_fold_random.py
+++ model_training_k_fold_random.py
@@ -12,7 +12,7 @@ import data_norm
 
 #Train Test Split parameters
 n = 8
-id_num = 'Guo_0409_Zoom_invert_'+str(n)+'fold'
+id_num = 'Guo_0409_Zoom_invert_combined_loss_'+str(n)+'fold'
 SEED = 932894
 #Confidence threshold for nuclei identification
 cutoff = 0.5
