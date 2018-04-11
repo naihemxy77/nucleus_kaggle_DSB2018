43869914c80263c70b963a6e742eed004a54e401
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-04-10 23:43:38 -0400

    Update ZoomNet_jac_0402.py

--- ZoomNet_jac_0402.py
+++ ZoomNet_jac_0402.py
@@ -8,30 +8,13 @@ import tensorflow as tf
 
 np.random.seed(534899574)
 
-def jaccard_coef(y_true, y_pred):
-    # __author__ = Vladimir Iglovikov
-    smooth = 1e-12
-    intersection = K.sum(y_true * y_pred, axis=[0, -1, -2])
-    sum_ = K.sum(y_true + y_pred, axis=[0, -1, -2])
-    jac = (intersection + smooth) / (sum_ - intersection + smooth)
-    return K.mean(jac)
-
-def jaccard_loss(y_true, y_pred):
-    return 1-jaccard_coef(y_true, y_pred)
-
 def iou(y_pred,y_true):
     logits=tf.reshape(y_pred, [-1])
     trn_labels=tf.reshape(y_true, [-1])
     inter=tf.reduce_sum(tf.multiply(logits,trn_labels))
     union=tf.reduce_sum(tf.subtract(tf.add(logits,trn_labels),tf.multiply(logits,trn_labels)))
-    loss=tf.divide(inter,union)#tf.subtract(tf.constant(1.0, dtype=tf.float32),tf.divide(inter,union))
+    loss=tf.subtract(tf.constant(1.0, dtype=tf.float32),tf.divide(inter,union))
     return loss
-
-def combined_loss(y_true, y_pred):
-    binary_loss = K.mean(K.binary_crossentropy(y_true, y_pred), axis=-1)#-K.mean(y_true*K.log(y_pred)+(1-y_true)*K.log(1-y_pred))
-    jac = iou(y_pred,y_true)
-    combined_loss = binary_loss-K.log(jac)
-    return combined_loss
 
 def cnn_block(X,f,k):
     X = Conv2D(filters=f, kernel_size=(k,k), kernel_initializer='he_normal', padding='same')(X)
@@ -60,7 +43,7 @@ def model_gen(InputDim):
     outputs = Conv2D(1,(1,1), kernel_initializer='he_normal', activation='sigmoid')(combined)
     model = Model(inputs=[inputs], outputs=[outputs])
     adam = Adam(lr=0.0005, decay=0.0, amsgrad=True)
-    model.compile(loss=combined_loss,
+    model.compile(loss=iou,
                   optimizer=adam,
                   metrics=['accuracy'])
     print('Model Construction Finished.')
