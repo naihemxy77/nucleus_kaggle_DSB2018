7b9d9f5c7cef2bbf9fdb3fe208ea8c0fd37f9e95
Songyao Ma <DassennMarbee@users.noreply.github.com>
2018-04-11 11:00:25 +0800

    Create trainLabel.sh
    For generating training labels

---
+++ trainLabel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+python3 model_test.py 0
+python3 model_test.py 1
+python3 model_test.py 2
+python3 model_test.py 3
+python3 model_test.py 4
+python3 model_test.py 5
+python3 model_test.py 6
