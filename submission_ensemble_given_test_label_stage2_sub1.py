5d099e36651fef6f4e5b052b0ff9d45a3cb7e145
longlongyunyunguoguo <36348086+longlongyunyunguoguo@users.noreply.github.com>
2018-04-14 18:20:48 -0400

    Update submission_ensemble_given_test_label_stage2_sub1.py

--- submission_ensemble_given_test_label_stage2_sub1.py
+++ submission_ensemble_given_test_label_stage2_sub1.py
@@ -20,21 +20,21 @@ cutoff = 0.5
 
 submissionfilename = 'Ensemble_stage2_submission1'
 
-start = NNN*500
-end = min((NNN+1)*500,3019)
+start = int(NNN)*500
+end = min((int(NNN)+1)*500,3019)
 #Best unet
 print('read in best unet results...')
-test_label_unet = pickle.load(open( "./inputs/UnetRes.p","rb" ))
+test_label_unet = pickle.load(open( "../inputs/UnetRes.p","rb" ))
 test_label_unet = pd.DataFrame(test_label_unet, columns=['ImageId','ImageOutput'])
 test_label_unet = test_label_unet.loc[start:end,:]
 #Best hollow
 print('read in best zoomnet results...')
-test_label_zoom = pickle.load(open( "./inputs/ZoomNetAllRes.p","rb" ))
+test_label_zoom = pickle.load(open( "../inputs/ZoomNetAllRes.p","rb" ))
 test_label_zoom = pd.DataFrame(test_label_zoom, columns=['ImageId','ImageOutput'])
 test_label_zoom = test_label_zoom.loc[start:end,:]
 #Best hollow
 print('read in best hollow results...')
-test_label_hollow = pickle.load(open( "./inputs/ZoomNetHollowRes.p","rb" ))
+test_label_hollow = pickle.load(open( "../inputs/ZoomNetHollowRes.p","rb" ))
 test_label_hollow = pd.DataFrame(test_label_hollow, columns=['ImageId','ImageOutput'])
 test_label_hollow = test_label_hollow.loc[start:end,:]
 
