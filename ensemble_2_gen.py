f560c5ce1b73bdd04d035437157afbfed5898bd5
Songyao Ma <DassennMarbee@users.noreply.github.com>
2018-04-16 18:26:54 +0800

    Update ensemble_2_gen.py

--- ensemble_2_gen.py
+++ ensemble_2_gen.py
@@ -1,3 +1,4 @@
+import sys
 import matplotlib
 matplotlib.use('Agg')
 import numpy as np
@@ -17,6 +18,18 @@ from scipy.ndimage import binary_fill_holes
 from data_norm import minmax_norm,invert_norm
 
 from skimage.filters import gaussian
+from skimage import img_as_float
+from cv2 import pyrUp,pyrDown
+from data_norm import minmax_norm
+
+NNN = int(sys.argv[1])
+
+def medianFilter(imgEx):
+    imgA = pyrUp(img_as_float(imgEx))
+    imgA[np.where(imgA<np.median(imgA))]=np.median(imgA)
+    imgB = pyrDown(imgA)
+    imgC = minmax_norm(imgB)
+    return imgC
 
 def getLocal(img):
     ch = rgb2gray(img[:,:,:3])
@@ -31,39 +44,48 @@ def getLocal(img):
     return tmp
 
 print('load test files...')
-test_df2 = pickle.load(open('./inputs/test_df2.p','rb'))
-test_stage2_ensemble2 = pickle.load(open('./inputs/test_stage2_ensemble2.p','rb'))
-
+test_df2 = pickle.load(open('../inputs/test_df2.p','rb'))
+test_stage2_ensemble2 = pickle.load(open('../inputs/test_stage2_ensemble2.p','rb'))
+print(test_df2.head(n=1))
 print('load base...')
-baseres = pd.read_pickle('./inputs/base_pred_stage2.p')#baseres
+baseres = pd.read_pickle('../inputs/base_pred_stage2.p')#baseres
 baseres = pd.DataFrame(baseres, columns=['ImageId','ImageLabel'])
+print(baseres.head(n=1))
 print('load unet...')
-unetres = pd.read_pickle('./inputs/UnetRes.p') #unetres
+unetres = pd.read_pickle('../inputs/UnetRes.p') #unetres
 unetres.drop(columns=['index'],inplace=True)
 unetres.rename(columns={'ImageOutput':'ImageLabel'},inplace=True)
+print(unetres.head(n=1))
 #unetres = pd.DataFrame(unetres, columns=['ImageId','ImageLabel'])
 print('load zoom...')
-zoomallres = pd.read_pickle('./inputs/ZoomNetAllRes.p') #zoomallres
+zoomallres = pd.read_pickle('../inputs/ZoomNetAllRes.p') #zoomallres
 zoomallres.drop(columns=['index'],inplace=True)
 zoomallres.rename(columns={'ImageOutput':'ImageLabel'},inplace=True)
+print(zoomallres.head(n=1))
 #zoomallres = pd.DataFrame(zoomallres, columns=['ImageId','ImageLabel'])
 print('load hollow...')
-hollowres = pd.read_pickle('./inputs/ZoomNetHollowRes.p')#hollowres
+hollowres = pd.read_pickle('../inputs/ZoomNetHollowRes.p')#hollowres
 hollowres.drop(columns=['index'],inplace=True)
 hollowres.rename(columns={'ImageOutput':'ImageLabel'},inplace=True)
+print(hollowres.head(n=1))
 #hollowres = pd.DataFrame(hollowres, columns=['ImageId','ImageLabel'])
 print('load new...')
-newunetres = pd.read_pickle('./inputs/NewUnetRes.p')#newunet
+newunetres = pd.read_pickle('../inputs/NewUnetRes.p')#newunet
 newunetres.drop(columns=['index'],inplace=True)
 newunetres.rename(columns={'ImageOutput':'ImageLabel'},inplace=True)
+print(newunetres.head(n=1))
 #newunetres = pd.DataFrame(newunetres, columns=['ImageId','ImageLabel'])
 
 
 final_label = []
-for img_index in range(test_df2.shape[0]):          
+iii=1
+for img_index in range(test_df2.shape[0]):
+    if iii<NNN*200:
+        iii+=1
+        continue
     img = test_df2.loc[img_index,'Image'][:,:,:3]
     img_id = test_df2.loc[img_index,'ImageId']
-
+    print(img_index)
     if test_df2.loc[test_df2.ImageId==img_id,'hsv_cluster'].item()==0:
         img = minmax_norm(img)
     else:
@@ -74,7 +96,7 @@ for img_index in range(test_df2.shape[0]):
     mask1 = unetres.loc[unetres.ImageId==img_id,'ImageLabel'].item().squeeze()
     mask2 = zoomallres.loc[zoomallres.ImageId==img_id,'ImageLabel'].item().squeeze()
     mask3 = hollowres.loc[hollowres.ImageId==img_id,'ImageLabel'].item().squeeze()
-    mask4 = newunetres.loc[newunetres.ImageId==img_id,'ImageLabel'].item().squeeze()
+    mask4 = medianFilter(newunetres.loc[newunetres.ImageId==img_id,'ImageLabel'].item().squeeze())
     mask5 = getLocal(img)
     #Weights for different models
     w0 = int(test_stage2_ensemble2.loc[test_stage2_ensemble2.ImageId==img_id,'baseline'].item())
@@ -96,6 +118,10 @@ for img_index in range(test_df2.shape[0]):
     
     new_mask = np.where(new_mask>0.5,1,0)
     final_label.append((img_id,new_mask))
+    iii+=1
+    if iii>(NNN+1)*200:
+        break
 
 test_pred_stage2_ensemble2 = pd.DataFrame(final_label, columns=['ImageId','ImageLabel'])
-pickle.dump( test_pred_stage2_ensemble2, open( "test_pred_stage2_ensemble2.p", "wb" ) )
+#pickle.dump( test_pred_stage2_ensemble2, open( "test_pred_stage2_ensemble2.p", "wb" ) )
+test_pred_stage2_ensemble2.to_pickle("Redo_ensemble2_"+str(NNN)+".p")
